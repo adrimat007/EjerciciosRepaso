aa62d6f75244945ede13a3844380882fac37e654
adrimat007 <adrimatgameamos@gmail.com>
2015-11-13 17:45:46 +0100

    Repaso 7 completado  cre cuadrados en culaquier lado de la pantalla, optimizar para que el cuadrado del centro no se relleno

---
+++ src/Repaso7.java
@@ -0,0 +1,73 @@
+import java.awt.event.MouseEvent;
+
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+/*
+ * Autor adrian ponce
+ * repaso 7 
+ * Cuando se hace clic en un rectangulo cental, genera un nuevo rectangulo aleatorio
+ * 
+ * 
+ */
+
+public class Repaso7 extends GraphicsProgram {
+
+
+	//declaro una varible de instancia para almacenar el rectangulo
+	//como es de instancia, se ve desde el ini y desde el run
+	GRect rectangulo;
+	GRect rectangulo2;
+	
+
+	//declaro una variable de tipo aleatorio 
+	RandomGenerator aleatorio = new RandomGenerator();
+
+	public void init(){
+		//cambiamos el tamaño de la pantalla
+		setSize(800,600);
+		//creo el rectangulo de 120 de acho por 80 alto
+		rectangulo = new GRect(120,80);
+		rectangulo2 = new GRect(0,0);
+		//inserto el "escuchador" del raton
+		addMouseListeners();
+
+
+
+	}
+
+
+	public void run(){
+
+		//añado el rectangulo en el centro exacto de la pantalla
+
+		add (rectangulo, getWidth()/2 - rectangulo.getWidth()/2, getHeight()/2 - rectangulo.getHeight()/2);
+		//add(rectangulo2);
+	}
+
+	
+	//añado el método que escucha el evento del clic del raton
+	
+	public void mouseClicked(MouseEvent evento){
+		// si en la psocicion dentro del rectangulo se cambie de collor
+		
+		// GObject choque = null;	
+		// choque = getElementAt(rectangulo.getWidth(),rectangulo.getHeight());		
+		// if (choque != rectangulo){
+		
+		if (getElementAt( evento.getX(), evento.getY()) == rectangulo){
+		//	int  altoCirculo = aleatorio.nextInt(20);
+		//	int posicion = aleatorio.nextInt(800);
+			GRect rectangulo3 = new GRect (aleatorio.nextInt(200) , aleatorio.nextInt(200) );
+			rectangulo3.setFilled(true);
+			rectangulo3.setFillColor(aleatorio.nextColor());
+		rectangulo.setFilled(true);
+		rectangulo.setFillColor(aleatorio.nextColor());
+		add(rectangulo3,aleatorio.nextInt(getWidth()), aleatorio.nextInt(getHeight()));
+		}
+	}
+	
+	
+	
+}
